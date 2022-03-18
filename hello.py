38897320e4659664b2630d9c2c05604999ee6ef7
ad-858 <aarti.dhiman858@gmail.com>
2022-03-18 17:41:04 +0530

    comments,print statement,escape sequence

---
+++ hello.py
@@ -0,0 +1,9 @@
+#This is a comment
+print("hello")
+"""
+Multiline comment
+"""
+print(" Hello",end=" ")
+print("World")
+#Escape sequence 
+print("hello everyone\n" "Hope you are doing great\t !!!")
