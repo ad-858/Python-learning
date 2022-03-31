98b5df61f01d4983098af3ee2fd83742ef837eb4
ad-858 <aarti.dhiman858@gmail.com>
2022-03-31 10:38:07 +0530

    type of function and casting of variables

--- variable.py
+++ variable.py
@@ -7,3 +7,12 @@ print(firstNumber,"\n",secondNumber)
 # assign multiple values to multiple variables
 a,b,c=1,3,5
 print(a,b,c)
+# assign multiple variables single value
+a=b=c= 5
+print(a,b,c)
+# type of function
+print(type(myFirstName))
+# casting of variable
+# print(str(a)+b)
+# to print a line multiple times
+print( 10*(myFirstName + "\n"))
