65f5a8f9866df4afa140af7e09870a4f96ded504
ad-858 <aarti.dhiman858@gmail.com>
2022-03-31 10:01:25 +0530

    variable declaration

---
+++ variable.py
@@ -0,0 +1,9 @@
+#vairaibles..............
+myFirstName="John"
+print(myFirstName)
+firstNumber=15
+secondNumber=45.5
+print(firstNumber,"\n",secondNumber)
+# assign multiple values to multiple variables
+a,b,c=1,3,5
+print(a,b,c)
