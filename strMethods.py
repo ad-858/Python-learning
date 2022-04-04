e58edf651e8f1f524820655d16f467510c5a47c3
ad-858 <aarti.dhiman858@gmail.com>
2022-04-04 14:52:50 +0530

    str methods like isalpha,index etc

--- strMethods.py
+++ strMethods.py
@@ -1,5 +1,19 @@
+"""
 myStr="Hello, and welcome to my world!"
 print(myStr.encode(encoding="ascii",errors="backslashes"))
 print(myStr.endswith("!"))
 str="H\te\tl\tlo"
 print(str.expandtabs(12))
+txt1="My name is {name} and I am {age}".format(name="Aarti",age=32)
+print(txt1)
+txt1="My name is {} and I am {}".format("Aarti Dhiman",32)
+print(txt1.find("i"))
+"""
+str="Hello, and welcome to my world!"
+print(str.index("and"))
+print(str.isalnum())
+print(str.isalpha())
+print(str.islower())
+print(str.isnumeric())
+print(str.istitle())
+print(str.isupper())
