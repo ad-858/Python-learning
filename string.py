ee3cb9524f07e835d7acb83d71bcb4692a085179
ad-858 <aarti.dhiman858@gmail.com>
2022-04-04 11:03:11 +0530

    string slicing

---
+++ string.py
@@ -0,0 +1,11 @@
+# string slicing
+myStr="Python is a good language"
+print(myStr[2:5])
+print(myStr[2:16])
+print(myStr[:])
+print(myStr[:10])
+print(myStr[0:])
+print(myStr[2:18:3])
+# negatative slicing
+print(myStr[-5:-1])
+print(myStr[::-1])
