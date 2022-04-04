f8382d8b2723a10637550bc3ad9f13555b4b9a6f
ad-858 <aarti.dhiman858@gmail.com>
2022-04-04 16:00:55 +0530

    join and partition string methods

--- strMethods.py
+++ strMethods.py
@@ -9,11 +9,20 @@ print(txt1)
 txt1="My name is {} and I am {}".format("Aarti Dhiman",32)
 print(txt1.find("i"))
 """
-str="Hello, and welcome to my world!"
-print(str.index("and"))
-print(str.isalnum())
-print(str.isalpha())
-print(str.islower())
-print(str.isnumeric())
-print(str.istitle())
-print(str.isupper())
+# str="Hello, and welcome to my world!"
+# print(str.index("and"))
+# print(str.isalnum())
+# print(str.isalpha())
+# print(str.islower())
+# print(str.isnumeric())
+# print(str.istitle())
+# print(str.isupper())
+myTuple = ("John", "Peter", "Vicky")
+
+x = " =".join(myTuple)
+
+print(x)
+myTuple="AppleBananaOranges"
+print(myTuple.partition("Banana"))
+print(myTuple.partition("Apple"))
+print(myTuple.partition("Oranges"))
