026df2e5d1d3f1cbb4a70828e91a42be657bacdc
ad-858 <aarti.dhiman858@gmail.com>
2022-04-05 15:28:27 +0530

    append and clear methods in list

---
+++ list-methods.py
@@ -0,0 +1,10 @@
+myList1=["Apple","Banana","Oranges"]
+myList2=["Grapes"]
+x1=myList1.append(myList2)
+print(myList1)
+print(x1)
+x="Dates"
+myList1.append(x)
+print(myList1)
+myList1.clear()
+print(myList1)
