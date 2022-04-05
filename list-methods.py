3ea3cbf04e73b9565692a39f0bcebcb924367325
ad-858 <aarti.dhiman858@gmail.com>
2022-04-05 16:10:20 +0530

    copy,extend and insert list methods

--- list-methods.py
+++ list-methods.py
@@ -1,10 +1,18 @@
 myList1=["Apple","Banana","Oranges"]
 myList2=["Grapes"]
-x1=myList1.append(myList2)
+# x1=myList1.append(myList2)
+# print(myList1)
+# print(x1)
+# x="Dates"
+# myList1.append(x)
+# print(myList1)
+# myList1.clear()
+# print(myList1)
+print(myList1.count("Banana"))
+z=myList1.copy()
+print(z)
+myList1.extend(myList2)
 print(myList1)
-print(x1)
-x="Dates"
-myList1.append(x)
+myList1.insert(1,"Grapes")
 print(myList1)
-myList1.clear()
-print(myList1)
+
