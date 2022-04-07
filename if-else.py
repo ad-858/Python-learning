83269c4aec977a6905edf3f7f862bf2ae262a874
ad-858 <aarti.dhiman858@gmail.com>
2022-04-07 19:52:09 +0530

    conditional statement

---
+++ if-else.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age"))
+if age==18:
+    print("Sorry, We cannot decide")
+
+elif age>18:
+    print("YOU are eligible to drive")
+
+else :
+    print("You are not eligible to drive")    
