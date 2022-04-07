f4fa7ff319617de749e0820c84d8bb637bc60ff8
ad-858 <aarti.dhiman858@gmail.com>
2022-04-07 20:19:50 +0530

    condtional

--- if-else.py
+++ if-else.py
@@ -2,8 +2,16 @@ age=int(input("Enter your age"))
 if age==18:
     print("Sorry, We cannot decide")
 
-elif age>18:
+elif (age>18 and age<100):
     print("YOU are eligible to drive")
 
 else :
     print("You are not eligible to drive")    
+
+   
+   
+   
+   
+   
+    
+    
