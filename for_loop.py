cb83e33fe82c099433f08c13c17090f10f7aeadb
ad-858 <aarti.dhiman858@gmail.com>
2022-04-08 16:12:45 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+str="Apple"
+for x in str:
+    print(x ,end=" ")
+
+list=["Apple ","Banana","Oranges","Grapes"]    
+for x in list:
+    print(x)
+myDict={
+    "name":"Aarohi",
+    "age ":32,
+    "place":"chandigarh"
+}    
+for x , y in myDict.items():
+    print(x,y)
+for x in myDict.values():
+    print(x)    
