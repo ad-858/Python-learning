60beb16fd0b86416d6673125e04747fbb6e5a8d7
ad-858 <aarti.dhiman858@gmail.com>
2022-04-14 15:19:22 +0530

    number is less than 100

--- while_loop.py
+++ while_loop.py
@@ -1,13 +1,19 @@
-i=1
-a=0
-while i<=100:
-    a=a+i
-    i=i+1
-print(a)
+# i=1
+# a=0
+# while i<=100:
+#     a=a+i
+#     i=i+1
+# print(a)
 
-lst=[10, 99, 98, 85, 45, 59, 65, 66, 76, 12, 35, 13, 100, 80, 95]
-i=0
-while i<len(lst):
-    if lst[i]==100:
-        print("the index of 100 is :"+ " "+str(i))
-    i=i+1
+# lst=[10, 99, 98, 85, 45, 59, 65, 66, 76, 12, 35, 13, 100, 80, 95]
+# i=0
+# while i<len(lst):
+#     if lst[i]==100:
+#         print("the index of 100 is :"+ " "+str(i))
+#     i=i+1
+n=int(input("Enter the number"))
+while n<=100:
+    if n<=100:
+        n=int(input("Enter the number"))
+    else:
+        break    
