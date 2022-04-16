6ddcceab006395e20d7f66990907148910ad939c
ad-858 <aarti.dhiman858@gmail.com>
2022-04-16 15:22:04 +0530

    doc string

--- my_function.py
+++ my_function.py
@@ -1,6 +1,8 @@
 def myFunction(a,b):
+    """ This is a function to add two numbers only"""
     sumOfNumbers=a+b
     return sumOfNumbers
+print(myFunction.__doc__)    
 print(myFunction(5,2))  
 #  args and kwargs
 
